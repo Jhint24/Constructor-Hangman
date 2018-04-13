40ccc33333aa0e566a4698bf713c119d0690420c
Josh <jhint2424@gmail.com>
2018-04-13 14:52:25 -0700

    update letter

---
+++ Letter.js
@@ -0,0 +1,10 @@
+function Letter (character, guessed,)   {
+    this.character = character,
+    this.guessed = guessed,
+    this.underscore = function ()   {
+
+    }
+    this.check = function ()    {
+        
+    }
+}  
