a15f54d60888a354f51f2094607c48407f2d7f87
Josh <jhint2424@gmail.com>
2018-04-15 20:24:53 -0700

    letter

--- Letter.js
+++ Letter.js
@@ -1,10 +1,17 @@
 function Letter (character, guessed,)   {
     this.character = character,
-    this.guessed = guessed,
+    this.guessed = false,
     this.underscore = function ()   {
-
+        if (this.guessed)   {
+            return this.character + " ";
+        }
+        else {
+            return "_"
+        }
     }
-    this.check = function ()    {
-        
-    }
-}  
+//     this.check = function (guess)    {
+//         if (guess === this.character)    {
+//             this.guessed = true;
+//         }
+//     }
+    }  
