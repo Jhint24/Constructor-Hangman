cf322fb70a9359eee88ef0bd62a042ae7b1ff244
Josh <jhint2424@gmail.com>
2018-04-15 20:44:53 -0700

    letter constructor

--- Letter.js
+++ Letter.js
@@ -1,17 +1,28 @@
+//Constructor built of the letter of the word 
 function Letter (character, guessed,)   {
     this.character = character,
     this.guessed = false,
+    //character correct or incorrect function 
     this.underscore = function ()   {
-        if (this.guessed)   {
-            return this.character + " ";
+        if (this.character === " ")   {
+            return " ";
         }
-        else {
-            return "_"
+        else if (this.guessed === true)  {
+            return "_ "
+        }
+        else { 
+            return this.character
         }
     }
-//     this.check = function (guess)    {
-//         if (guess === this.character)    {
-//             this.guessed = true;
-//         }
-//     }
-    }  
+
+//function that takes a character as an argument and calls the guess function
+    this.checkChar = function (guess)   {
+        if (guess === this.character)   {
+            this.guessed = true
+        }
+
+        return this.underscore();
+    }
+}  
+
+module.exports = Letter;
