4098c0ed0446c311ea65beb8a7f82e41f103e5c5
Josh <jhint2424@gmail.com>
2018-04-15 21:52:23 -0700

    logic started

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
 var Word = require ("./Word");
 var inquirer = require("inquirer");
 //remaining guesses
-//prompts user for guess
+var guessRemain = 10;
 //randomly selects a word
 //word bank
+var word = new Word("play");
+var golfWords = ["green", "fairway", "putter", "sandtrap", "bunker", "hazard", "driver", "wedge", "caddy", "clubhouse", "golfcart", "teebox", "iron", "range", "fore", "mulligan", "provisional"];
+var wordMath = Math.floor(Math.random() * (golfWords.length + 1));
+//prompts user for guess
 //use inquirer
